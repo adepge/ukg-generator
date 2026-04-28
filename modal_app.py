18d17de7061786b6a165995f702bda7eb9d5ac74
Adam George <adepge@gmail.com>
2026-04-28 13:27:09 +0100

    Reduce scaledown time to default

--- modal_app.py
+++ modal_app.py
@@ -39,7 +39,7 @@ app = modal.App("ukg-generator")
 @app.cls(
     image=image,
     gpu="T4",
-    scaledown_window=300,
+    scaledown_window=60,
     timeout=900,
     enable_memory_snapshot=True,
     # Set `min_containers=1` here if you want to eliminate cold starts
